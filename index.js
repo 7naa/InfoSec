63d380973a81bb79609f9c2b234eb57fc2179321
yna <b022210245@student.utem.edu.my>
2025-01-09 12:42:31 +0800

    add swagger code for user register mongo 3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const uri = "mongodb+srv://7naa:perempuancantik@infosec.v4tpw.mongodb.net/?retryWrites=true&w=majority&appName=InfoSec";
+const uri = "mongodb+srv://7naa:perempuancantik@infosec.v4tpw.mongodb.net/";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
